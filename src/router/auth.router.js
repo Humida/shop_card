72dad441578d2d812f5b72197f51a331bff14f4b
Humida <63026473+Humida@users.noreply.github.com>
2021-09-22 21:13:53 +0700

    Delete src directory

--- src/router/auth.router.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../app/controller/auth.controller')
-
-router.post('/', auth.login);
-module.exports = router;
--- src/router/home.router.js
+++
@@ -1,7 +0,0 @@
-const home = require('../app/controller/items.controller');
-const express = require('express');
-const router = express.Router();
-
-router.get('/', home.home);
-
-module.exports = router;
